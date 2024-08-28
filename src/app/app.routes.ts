e5d711346ddcb5e5419f20a59e425bc22329d590
Didier <Didier@Didier.local>
2024-08-27 23:14:25 -0500

    Semana 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { TeamComponent } from './pages/team/team.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "home", component: HomeComponent },
+    { path: "aboutus", component: AboutusComponent },
+    { path: "contactus", component: ContactusComponent },
+    { path: "team", component: TeamComponent }
+];
