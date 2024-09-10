d5b742d9aebab66f0bd2b08dcb021ac52eb83ff5
Didier <Didier@Didier.local>
2024-09-10 09:55:40 -0500

    Avance de la semana 2 en clases, se cambió el contact us para crear un formulario y mostrar el mapa

--- src/app/pages/contactus/contactus.component.ts
+++ src/app/pages/contactus/contactus.component.ts
@@ -1,12 +1,34 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contactus',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './contactus.component.html',
   styleUrl: './contactus.component.css'
 })
 export class ContactusComponent {
+  constructor(
+    private fb: FormBuilder
+  ){}
 
+  contactusForm = this.fb.group({
+    persona: this.fb.group({
+      nombre: ["", Validators.required],
+      apellido: ["", Validators.required],
+    }),
+    correo: ["", [Validators.email, Validators.required]],
+    mensaje: ["", Validators.required]
+  })
+
+  onSubmit(){
+    if (this.contactusForm.valid){
+      console.log(this.contactusForm.value)
+    } else {
+      alert("Formulario no válido")
+    }
+    
+  }
 }
